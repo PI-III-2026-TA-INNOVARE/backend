f45995a541f9820032792fa9cda68e22ad87ff87
Queren <querenhalves@outlook.com>
2026-04-27 13:58:37 -0300

    permissão para visualização de universidades

--- apps/universities/views.py
+++ apps/universities/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from apps.universities.models import University
 from apps.universities.serializers import UniversitySerializer
 
@@ -6,6 +6,7 @@ from apps.universities.serializers import UniversitySerializer
 class UniversityCreateListView(generics.ListCreateAPIView):
     queryset = University.objects.all()
     serializer_class = UniversitySerializer
+    permission_classes = [permissions.AllowAny]
 
 class UniversityRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = University.objects.all()
